6ebddbf36088e97f4d5e4ad3a0b5ee7c35672a71
vtv2186 <vishnu.vithala@gmail.com>
2023-03-27 21:41:33 -0700

    Update app_deepspeech1.py

--- app_deepspeech1.py
+++ app_deepspeech1.py
@@ -176,7 +176,7 @@ def app_sst(model_path: str, lm_path: str, lm_alpha: float, lm_beta: float, beam
                 sound_chunk = sound_chunk.set_channels(1).set_frame_rate(
                     model.sampleRate()
                 )
-                buffer = np.array(sound_chunk.get_array_of_samples())
+                buffer = np.array(sound_chunk.get_array_of_samples()).astype(np.float32)
                 #librosa.load(buffer)
                 y, index = librosa.effects.trim(buffer)
                 D = librosa.stft(buffer)
