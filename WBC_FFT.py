d7f8ef8147d2618d30a1f3ed689743c44a031c06
vtv2186 <vishnu.vithala@gmail.com>
2023-04-06 13:55:34 -0700

    Update WBC_FFT.py

--- WBC_FFT.py
+++ WBC_FFT.py
@@ -52,59 +52,59 @@ def main():
     # sound_window_buffer = None
     
     
-    # with server_state_lock["webrtc_contexts"]:
-    #     webrtc_contexts = server_state["webrtc_contexts"]
-    #     if self_ctx.state.playing and self_ctx not in webrtc_contexts:
-    #         webrtc_contexts.append(self_ctx)
-    #         server_state["webrtc_contexts"] = webrtc_contexts
-    #     elif not self_ctx.state.playing and self_ctx in webrtc_contexts:
-    #         webrtc_contexts.remove(self_ctx)
-    #         server_state["webrtc_contexts"] = webrtc_contexts
+    with server_state_lock["webrtc_contexts"]:
+        webrtc_contexts = server_state["webrtc_contexts"]
+        if self_ctx.state.playing and self_ctx not in webrtc_contexts:
+            webrtc_contexts.append(self_ctx)
+            server_state["webrtc_contexts"] = webrtc_contexts
+        elif not self_ctx.state.playing and self_ctx in webrtc_contexts:
+            webrtc_contexts.remove(self_ctx)
+            server_state["webrtc_contexts"] = webrtc_contexts
 
-    # active_other_ctxs = [
-    #     ctx for ctx in webrtc_contexts if ctx != self_ctx and ctx.state.playing
-    # ]
+    active_other_ctxs = [
+        ctx for ctx in webrtc_contexts if ctx != self_ctx and ctx.state.playing
+    ]
 
-    # for ctx in active_other_ctxs:
-    #     webrtc_streamer(
-    #         key=str(id(ctx)),
-    #         mode=WebRtcMode.SENDRECV,
-    #         client_settings=ClientSettings(
-    #             rtc_configuration={  # Add this line
-    #       # "iceServers": [{"urls": ["stun:stun4.l.google.com:19302"]}]
-    #       # "iceServers": [{   "urls": [ "stun:ws-turn1.xirsys.com" ]}, {   "username": "_gOvGuKm6kPUXW7I78axfsf8e7hlY2VaJziOfzYjFnnEZqUb50vvQhQQzevloqKTAAAAAGQc1ox2aXNobnV0ZWph",   "credential": "6b2aa7c4-c9cc-11ed-b509-0242ac140004",   "urls": [       "turn:ws-turn1.xirsys.com:80?transport=udp",       "turn:ws-turn1.xirsys.com:3478?transport=udp",       "turn:ws-turn1.xirsys.com:80?transport=tcp",       "turn:ws-turn1.xirsys.com:3478?transport=tcp",       "turns:ws-turn1.xirsys.com:443?transport=tcp",       "turns:ws-turn1.xirsys.com:5349?transport=tcp"   ]}]
-    #       "iceServers": [{
-    #          "urls": [ "stun:ws-turn4.xirsys.com" ]
-    #       }, {
-    #          "username": "UIvu1OpNVH8Aw_IWuAYaSU2o6WaTD2hyykLgfqkO563ivxUWWAfnguGDIar3AaoaAAAAAGQrHyp2aXNobnV0ZWph",
-    #          "credential": "eebe884a-d24f-11ed-9d96-0242ac140004",
-    #          "urls": [
-    #              "turn:ws-turn4.xirsys.com:80?transport=udp",
-    #              "turn:ws-turn4.xirsys.com:3478?transport=udp",
-    #              "turn:ws-turn4.xirsys.com:80?transport=tcp",
-    #              "turn:ws-turn4.xirsys.com:3478?transport=tcp",
-    #              "turns:ws-turn4.xirsys.com:443?transport=tcp",
-    #              "turns:ws-turn4.xirsys.com:5349?transport=tcp"
-    #          ]
-    #       }]
+    for ctx in active_other_ctxs:
+        webrtc_streamer(
+            key=str(id(ctx)),
+            mode=WebRtcMode.SENDRECV,
+            client_settings=ClientSettings(
+                rtc_configuration={  # Add this line
+          # "iceServers": [{"urls": ["stun:stun4.l.google.com:19302"]}]
+          # "iceServers": [{   "urls": [ "stun:ws-turn1.xirsys.com" ]}, {   "username": "_gOvGuKm6kPUXW7I78axfsf8e7hlY2VaJziOfzYjFnnEZqUb50vvQhQQzevloqKTAAAAAGQc1ox2aXNobnV0ZWph",   "credential": "6b2aa7c4-c9cc-11ed-b509-0242ac140004",   "urls": [       "turn:ws-turn1.xirsys.com:80?transport=udp",       "turn:ws-turn1.xirsys.com:3478?transport=udp",       "turn:ws-turn1.xirsys.com:80?transport=tcp",       "turn:ws-turn1.xirsys.com:3478?transport=tcp",       "turns:ws-turn1.xirsys.com:443?transport=tcp",       "turns:ws-turn1.xirsys.com:5349?transport=tcp"   ]}]
+          "iceServers": [{
+              "urls": [ "stun:ws-turn4.xirsys.com" ]
+          }, {
+              "username": "UIvu1OpNVH8Aw_IWuAYaSU2o6WaTD2hyykLgfqkO563ivxUWWAfnguGDIar3AaoaAAAAAGQrHyp2aXNobnV0ZWph",
+              "credential": "eebe884a-d24f-11ed-9d96-0242ac140004",
+              "urls": [
+                  "turn:ws-turn4.xirsys.com:80?transport=udp",
+                  "turn:ws-turn4.xirsys.com:3478?transport=udp",
+                  "turn:ws-turn4.xirsys.com:80?transport=tcp",
+                  "turn:ws-turn4.xirsys.com:3478?transport=tcp",
+                  "turns:ws-turn4.xirsys.com:443?transport=tcp",
+                  "turns:ws-turn4.xirsys.com:5349?transport=tcp"
+              ]
+          }]
         
       
-    #     },
-    #             media_stream_constraints={
-    #                 "video": True,
-    #                 "audio": True,
-    #             },
-    #         ),
-    #         source_audio_track=ctx.output_audio_track,
-    #         source_video_track=ctx.output_video_track,
-    #         desired_playing_state=ctx.state.playing,
-    #     )
+        },
+                media_stream_constraints={
+                    "video": True,
+                    "audio": True,
+                },
+            ),
+            source_audio_track=ctx.output_audio_track,
+            source_video_track=ctx.output_video_track,
+            desired_playing_state=ctx.state.playing,
+        )
     fig_place = st.empty()
     fig, [ax_time, ax_freq] = plt.subplots(2, 1, gridspec_kw={"top": 1.5, "bottom": 0.2})
     sound_window_len = 5000  # 5s
     sound_window_buffer = None
     while True:
-        if self_ctx.audio_receiver:
+        if active_other_ctxs.audio_receiver:
             try:
                 audio_frames = self_ctx.audio_receiver.get_frames(timeout=1)
             except queue.Empty:
