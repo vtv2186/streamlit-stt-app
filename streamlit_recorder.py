0f7e85ca19040bdcb20acc5fc9088546f9d72720
vtv2186 <vishnu.vithala@gmail.com>
2023-04-21 21:51:08 -0700

    Update streamlit_recorder.py

--- streamlit_recorder.py
+++ streamlit_recorder.py
@@ -50,7 +50,10 @@ def aiortc_audio_recorder(wavpath):
     def recorder_factory():
         return MediaRecorder(wavpath)
 
-    webrtc_ctx: WebRtcStreamerContext = webrtc_streamer(
+        if "webrtc_contexts" not in server_state:
+         server_state["webrtc_contexts"] = []   
+ 
+        webrtc_ctx: WebRtcStreamerContext = webrtc_streamer(
         key="sendonly-audio",
         mode=WebRtcMode.SENDONLY,
         #mode=WebRtcMode.SENDRECV,
@@ -78,7 +81,46 @@ def aiortc_audio_recorder(wavpath):
 
         ),
                )
-    
+        with server_state_lock["webrtc_contexts"]:
+             webrtc_contexts = server_state["webrtc_contexts"]
+             if webrtc_ctx.state.playing and webrtc_ctx not in webrtc_contexts:
+                 webrtc_contexts.append(webrtc_ctx)
+                 server_state["webrtc_contexts"] = webrtc_contexts
+             elif not webrtc_ctx.state.playing and webrtc_ctx in webrtc_contexts:
+                 webrtc_contexts.remove(webrtc_ctx)
+                 server_state["webrtc_contexts"] = webrtc_contexts
+                 
+                 
+        active_other_ctxs = [
+             ctx for ctx in webrtc_contexts if ctx != webrtc_ctx and ctx.state.playing
+         ]
+         
+        for ctx in active_other_ctxs:
+             webrtc_streamer(
+                 key=str(id(ctx)),
+                 mode=WebRtcMode.SENDONLY,
+                 client_settings=ClientSettings(
+                 rtc_configuration={
+                    # "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+                    "iceServers": [{
+                       "urls": [ "stun:ws-turn4.xirsys.com" ]
+                    }, {
+                       "username": "UIvu1OpNVH8Aw_IWuAYaSU2o6WaTD2hyykLgfqkO563ivxUWWAfnguGDIar3AaoaAAAAAGQrHyp2aXNobnV0ZWph",
+                       "credential": "eebe884a-d24f-11ed-9d96-0242ac140004",
+                       "urls": [
+                           "turn:ws-turn4.xirsys.com:80?transport=udp",
+                           "turn:ws-turn4.xirsys.com:3478?transport=udp",
+                           "turn:ws-turn4.xirsys.com:80?transport=tcp",
+                           "turn:ws-turn4.xirsys.com:3478?transport=tcp",
+                           "turns:ws-turn4.xirsys.com:443?transport=tcp",
+                           "turns:ws-turn4.xirsys.com:5349?transport=tcp"
+                       ]
+                    }]
+                        },
+                        media_stream_constraints=MEDIA_STREAM_CONSTRAINTS,
+
+                 ),
+                        )
    
 def save_frames_from_audio_receiver(wavpath):
     webrtc_ctx = webrtc_streamer(
@@ -88,46 +130,7 @@ def save_frames_from_audio_receiver(wavpath):
        # media_stream_constraints=MEDIA_STREAM_CONSTRAINTS,
     )
     
-    with server_state_lock["webrtc_contexts"]:
-        webrtc_contexts = server_state["webrtc_contexts"]
-        if webrtc_ctx.state.playing and webrtc_ctx not in webrtc_contexts:
-            webrtc_contexts.append(webrtc_ctx)
-            server_state["webrtc_contexts"] = webrtc_contexts
-        elif not webrtc_ctx.state.playing and webrtc_ctx in webrtc_contexts:
-            webrtc_contexts.remove(webrtc_ctx)
-            server_state["webrtc_contexts"] = webrtc_contexts
-            
-            
-    active_other_ctxs = [
-        ctx for ctx in webrtc_contexts if ctx != webrtc_ctx and ctx.state.playing
-    ]
-    
-    for ctx in active_other_ctxs:
-        webrtc_streamer(
-            key=str(id(ctx)),
-            mode=WebRtcMode.SENDONLY,
-            client_settings=ClientSettings(
-            rtc_configuration={
-               # "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-               "iceServers": [{
-                  "urls": [ "stun:ws-turn4.xirsys.com" ]
-               }, {
-                  "username": "UIvu1OpNVH8Aw_IWuAYaSU2o6WaTD2hyykLgfqkO563ivxUWWAfnguGDIar3AaoaAAAAAGQrHyp2aXNobnV0ZWph",
-                  "credential": "eebe884a-d24f-11ed-9d96-0242ac140004",
-                  "urls": [
-                      "turn:ws-turn4.xirsys.com:80?transport=udp",
-                      "turn:ws-turn4.xirsys.com:3478?transport=udp",
-                      "turn:ws-turn4.xirsys.com:80?transport=tcp",
-                      "turn:ws-turn4.xirsys.com:3478?transport=tcp",
-                      "turns:ws-turn4.xirsys.com:443?transport=tcp",
-                      "turns:ws-turn4.xirsys.com:5349?transport=tcp"
-                  ]
-               }]
-                   },
-                   media_stream_constraints=MEDIA_STREAM_CONSTRAINTS,
-
-            ),
-                   )
+   
            # client_settings=ClientSettings(
             #    rtc_configuration={  # Add this line
           # "iceServers": [{"urls": ["stun:stun4.l.google.com:19302"]}]
